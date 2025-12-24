2202364db580589a6e4ff87fef524e68ab4cdb60
Gabriel Alzina Alomar <44143564+bielalzina@users.noreply.github.com>
2025-12-24 11:09:10 +0100

    crea

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# 1. CONFIGURACIÓ INICIAL DE LA PÀGINA
+# Aquestes instruccions sempre han d'estar en l'inici.ArithmeticError
+# Definim el títol de la pàgina (pipella browser)
+
+st.set_page_config(
+    page_title="Corrector DC",
+    page_icon="🎓",
+    layout="wide",  
+)
